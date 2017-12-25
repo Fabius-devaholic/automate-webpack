1678e2cff2ef4aa1dc838e0ee0a8e3d9e469d32e
Hieu Tran <hieu@construct.sg>
2017-12-26 00:39:40 +0700

    Build scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,41 @@
 var path = require('path');
 var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 
 module.exports = {
   watch: true,
-  entry: './src/js/scripts.js',
+  entry: ['./src/scss/styles.scss', './src/js/scripts.js'],
   output: {
-    path: path.resolve(__dirname, 'built/js'),
-    filename: 'scripts.js'
+    path: path.resolve(__dirname, 'built'),
+    filename: 'js/scripts.js'
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015']
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['es2015']
+          }
         }
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
       }
     ]
   },
+  plugins: [
+    new ExtractTextPlugin({ // define where to save the file
+      filename: 'css/styles.css',
+      allChunks: true,
+    }),
+  ],
   stats: {
     colors: true
   },
